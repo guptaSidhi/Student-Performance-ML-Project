49956b62e40cbe5535f5a972e707f90eedfc79a0
Sidhi Gupta <guptasidhi159@gmail.com>
2024-11-02 14:57:09 +0530

    Setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+# This is basically used to make our application as a package itself.
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''This Function is returning the list of all libraries used in Project'''
+    requirements = []
+    with open(file_path) as file:
+        requirements = file.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+        
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
+setup(
+    name='StudentPerformaceMLProject',
+    version='0.0.1',
+    author='SidhiGupta',
+    author_email='guptasidhi159@gmail.com',
+    packages=find_packages(),
+    intstall_requires=get_requirements('requirements.txt')
+)
